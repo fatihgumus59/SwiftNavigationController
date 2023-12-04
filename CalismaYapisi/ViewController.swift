7fd228cbe611ee0fbd2c8cae3f7e08d963251aa1
Fatih Gumus <fatihgumus72@gmail.com>
2023-12-04 22:07:12 +0300

    Initial Commit

---
+++ CalismaYapisi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalismaYapisi
+//
+//  Created by Fatih Gümüş on 4.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
