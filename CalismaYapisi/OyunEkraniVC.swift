dfb3ee726217ad118009286c64e32c2d2b4d19ac
Fatih Gumus <fatihgumus72@gmail.com>
2023-12-05 20:57:12 +0300

    finish project

---
+++ CalismaYapisi/OyunEkraniVC.swift
@@ -0,0 +1,19 @@
+//
+//  OyunEkranVC.swift
+//  CalismaYapisi
+//
+//  Created by Fatih Gümüş on 5.12.2023.
+//
+
+import UIKit
+
+class OyunEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ CalismaYapisi/SonucEkraniVC.swift
@@ -0,0 +1,19 @@
+//
+//  SonucEkranıVC.swift
+//  CalismaYapisi
+//
+//  Created by Fatih Gümüş on 5.12.2023.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
